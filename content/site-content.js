b443da9d29036303b7de5c135a03f37fc78a106d
Ian M <imatlak03@gmail.com>
2026-07-04 16:59:31 -0400

    Update headline and scholar contact link

--- content/site-content.js
+++ content/site-content.js
@@ -259,7 +259,7 @@ window.LAB_CONTENT = {
   contact: [
     ["Email", "shanshan.liu@yale.edu", "mailto:shanshan.liu@yale.edu"],
     ["Affiliation", "Department of Cell Biology, Yale School of Medicine", null],
-    ["Google Scholar", "Scholar profile", "https://scholar.google.com/citations?user=nDvllAYAAAAJ&hl=en"],
+    ["Google Scholar", "Google Scholar", "https://scholar.google.com/citations?user=nDvllAYAAAAJ&hl=en"],
     ["ORCID", "0000-0001-8293-1025", "https://orcid.org/0000-0001-8293-1025"]
   ]
 };
