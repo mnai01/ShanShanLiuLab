e02597545a181e93ffa321b7000e8af620f1a5f6
shanshanliu1218-oss <shanshanliu1218@gmail.com>
2026-07-30 18:22:28 -0400

    Link Cell Biology department in Contact

--- content/site-content.js
+++ content/site-content.js
@@ -266,7 +266,7 @@ window.LAB_CONTENT = {
   ],
   contact: [
     ["Email", "shanshan.liu@yale.edu", "mailto:shanshan.liu@yale.edu"],
-    ["Affiliation", "Department of Cell Biology, Yale School of Medicine", null],
+    ["Affiliation", "Department of Cell Biology, Yale School of Medicine", "https://medicine.yale.edu/cellbio/"],
     ["Google Scholar", "Google Scholar", "https://scholar.google.com/citations?user=nDvllAYAAAAJ&hl=en"],
     ["ORCID", "0000-0001-8293-1025", "https://orcid.org/0000-0001-8293-1025"]
   ]
